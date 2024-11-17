83a69996d1acb546709614c9e79f494421bc633c
berk <sberkerduran99@gmail.com>
2024-11-17 19:19:54 +0300

    ilk ders

--- 01_MainSubjects/Program.cs
+++ 01_MainSubjects/Program.cs
@@ -10,6 +10,56 @@ namespace _01_MainSubjects
     {
         static void Main(string[] args)
         {
+            #region YazdırmaKomutları
+            // Console.WriteLine("***** Yemek Kategorileri ***** ");
+            // Console.WriteLine();
+            // Console.WriteLine("1-Çorbalar");
+            // Console.WriteLine("2-Ana Yemekler");
+            // Console.WriteLine("3-Soğuk başlangıçlar");
+            // Console.WriteLine("4-Salatalar");
+            // Console.WriteLine("5-Tatlılar");
+            // Console.WriteLine("6-İçecekler");
+            // Console.WriteLine();
+            // Console.WriteLine("***** Yemek Kategorileri ***** ");
+            // Console.Read();
+
+            #endregion
+
+            #region StringDeğişkenler
+            //string 
+            //Değişken_türü değişken_adı
+            //string name = "Berk";
+            //Console.WriteLine(name);
+
+            //string customerName ;
+            //string customerSurname  ;
+            //string customerPhone; 
+            //string customerEmail,district,city;
+
+            //customerName = "Berk";
+            //customerSurname = "Erduran";
+            //customerPhone = "+90 500 100 24 36 ";
+            //customerEmail = "sberkerduran99@gmail.com";
+            //district = "Kadıköy";
+            //city = "İstanbul";
+
+            //Console.WriteLine("***** Rezervasyon kartı *****");
+            //Console.WriteLine();
+            //Console.WriteLine("-------------------------------------------");
+            //Console.WriteLine("Müşteri : "+customerName+ " " + customerSurname);
+            //Console.WriteLine("İletişim : " + customerPhone);
+            //Console.WriteLine("Email Adresi : " + customerEmail);
+            //Console.WriteLine("Adres : " + district+ "/" + city);
+            //Console.WriteLine("-------------------------------------------");
+
+            #endregion
+            #region İntDeğişkenler
+
+
+            #endregion
+
+
+
         }
     }
 }
