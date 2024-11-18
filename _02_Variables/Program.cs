d0af5842475c2ee236fb7886714a2d754f1bc3fd
berk <sberkerduran99@gmail.com>
2024-11-18 20:15:55 +0300

    class 2

---
+++ _02_Variables/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region DoubleDeğişkenler
+            //double number;
+            //number = 4.85;
+            //Console.WriteLine(number);
+
+            //Console.WriteLine("***** Fiyat Listesi *****");
+            //Console.WriteLine();
+            //double applePrice, orangePrice, strawberyPrice, potatoPrice, tomatoPrice;
+            //applePrice = 14.85;
+            //orangePrice = 20.95;
+            //strawberyPrice = 45;
+            //potatoPrice = 9.74;
+            //tomatoPrice = 6.88;
+
+            //Console.WriteLine("---- Elma birim fiyatı:" + applePrice + " TL ");
+            //Console.WriteLine("---- Portakal birim fiyatı:" + orangePrice + " TL ");
+            //Console.WriteLine("---- Çilek birim fiyatı:" + strawberyPrice + " TL ");
+            //Console.WriteLine("---- Patates birim fiyatı:" + applePrice + " TL ");
+            //Console.WriteLine("---- Domates birim fiyatı:" + applePrice + " TL ");
+
+            //double appleGram, orangeGram, strawGram, potatoGram, tomatoGram;
+
+            //appleGram = 1.245;
+            //orangeGram = 2.650;
+            //strawGram = 0.750;
+            //potatoGram = 4.859;
+            //tomatoGram = 3.745;
+
+            //double appleTotalPrice = appleGram * applePrice;
+            //double orangeTotalPrice = orangeGram * orangePrice;
+            //double strawberryTotalPrice = strawGram * strawberyPrice;
+            //double potatoTotalPrice = potatoGram * potatoPrice;
+            //double tomatoTotalPrice = tomatoGram * tomatoPrice;
+
+            //Console.WriteLine("Alınan ürün: Elma - " + "Birim fiyarı: " + applePrice + " - Gramaj: " + appleGram + " - Toplam Tutar: " + appleTotalPrice);
+            //Console.WriteLine("Alınan ürün: Elma - " + "Birim fiyarı: " + orangePrice + " - Gramaj: " + orangeGram + " - Toplam Tutar: " + orangeTotalPrice);
+            //Console.WriteLine("Alınan ürün: Elma - " + "Birim fiyarı: " + strawberyPrice + " - Gramaj: " + strawGram + " - Toplam Tutar: " + strawberryTotalPrice);
+            //Console.WriteLine("Alınan ürün: Elma - " + "Birim fiyarı: " + potatoPrice + " - Gramaj: " + potatoGram + " - Toplam Tutar: " + potatoTotalPrice);
+            //Console.WriteLine("Alınan ürün: Elma - " + "Birim fiyarı: " + tomatoPrice + " - Gramaj: " + tomatoGram + " - Toplam Tutar: " + tomatoTotalPrice);
+            //double shoppingTotalPrice = appleTotalPrice+orangeTotalPrice+strawberryTotalPrice+potatoTotalPrice+tomatoTotalPrice;
+            //Console.WriteLine("Toplam Ücret: "+ shoppingTotalPrice+"TL");
+
+
+            #endregion
+
+            #region CharDeğişkenler 
+
+            char symbol;
+
+
+            #endregion
+
+
+            #region Klavyeden Veri Girişleri ve String değişkenler 
+
+            //Console.WriteLine("**** CSharp Hava Yolları Yolcu Bilgisi ****");
+            //Console.WriteLine();
+
+            //string passengerName, passengerSurname, passengerDistrict, passengerCity, passengerAge, passengerIdentityNumber;
+
+
+            //Console.WriteLine("Yolcu Adı: ");
+            //passengerName = Console.ReadLine();
+
+            //Console.WriteLine("Yolcu Soyadı: "); 
+            //passengerSurname = Console.ReadLine();
+            //Console.WriteLine("İlçe Bilgisi: ");
+            //passengerDistrict = Console.ReadLine();
+
+            //Console.WriteLine("Şehir Bilgisi: ");
+            //passengerCity = Console.ReadLine();
+
+            //Console.WriteLine("Yolcu Yaş: ");
+            //passengerAge = Console.ReadLine();
+
+            //Console.WriteLine("Yolcu TC Kimlik Numarsı: ");
+            //passengerIdentityNumber = Console.ReadLine();
+
+
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("----------------------------");
+            //Console.WriteLine("Yolcu TC Kimlik No: " + passengerIdentityNumber + " - Yolcu Ad Soyad: " + passengerName +" "+ passengerSurname + 
+            //    " " + passengerDistrict + " / " + passengerCity+ " / " + passengerAge);
+
+
+            #endregion
+
+            #region Klavyeden Tam Sayı Girişleri ve Dönüşümler
+
+             
+
+            #endregion
+            Console.Read();
+
+            
+
+        }
+    }
+}
