45c8d545f63ba2f8cfb5cc3913b5d24d0323b489
berk <sberkerduran99@gmail.com>
2024-11-22 18:57:19 +0300

    camp5

---
+++ 05_LoopsWitsStars/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace _05_LoopsWitsStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Alt alta on tane yıldız oluşturma 
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine('*');
+            //}
+
+            #endregion
+            #region Alt alta on tane yıldız oluşturma 
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write("*");s
+            //}
+
+            #endregion
+            #region Alt alta on tane yıldız oluşturma her satırda 10 tane yıldız olsun 
+
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("**********");
+            //}
+
+            #endregion
+            #region dik üçgen 
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            #endregion
+            #region  ters dik üçgen 
+
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            #endregion
+            #region  dik ve ters dik üçgen  beraber
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int i = 4; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
